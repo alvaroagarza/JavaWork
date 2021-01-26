3c618fd219e2b631e4f76f5b30b419abc0466e47
Alvaro Garza <meg4byt3@gmail.com>
2021-01-26 14:13:46 -0600

    refactor lotto method

--- controlFlow/src/com/tts/Main.java
+++ controlFlow/src/com/tts/Main.java
@@ -1,10 +1,11 @@
 package com.tts;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+
 
 
 
 public class Main {
+
     public static int random1 = (int) (Math.random() * 10);
     public static int random2 = (int) (Math.random() * 10);
     public static int random3 = (int) (Math.random() * 10);
@@ -57,42 +58,70 @@ public class Main {
 
             do {
                 System.out.println("What is the name of your favorite pet?:");
+                while (scanner.hasNextInt()){
+                    System.out.println("Invalid input. Please try again:");
+                    scanner.next();
+                }
                 petName = scanner.next();
-
             } while (petName.isEmpty());
 
             do {
                 System.out.println("What is their age?:");
+                while (!scanner.hasNextInt()){
+                    System.out.println("Invalid input. Please try again:");
+                    scanner.next();
+                }
+
                 petAge = scanner.nextInt();
 
             } while (petAge == 0);
 
             do {
                 System.out.println("What is your lucky number?:");
+                while (!scanner.hasNextInt()){
+                    System.out.println("Invalid input. Please try again:");
+                    scanner.next();
+                }
                 luckyNum = scanner.nextInt();
 
             } while (luckyNum == 0);
 
             do {
                 System.out.println("What is your favorite quarterback's number?:");
+                while (!scanner.hasNextInt()) {
+                    System.out.println("Invalid input. Please try again:");
+                    scanner.next();
+                }
                 qbNum = scanner.nextInt();
 
             } while (qbNum == 0);
 
             do {
                 System.out.println("What is the two digit model year of your car?:");
+                while (!scanner.hasNextInt()){
+                    System.out.println("Invalid input. Please try again:");
+                    scanner.next();
+                }
                 carYear = scanner.nextInt();
 
             } while (carYear == 0);
 
             do {
                 System.out.println("What is the first name of your favorite actor/actress?:");
+                while (scanner.hasNextInt()){
+                    System.out.println("Invalid input. Please try again:");
+                    scanner.next();
+                }
                 favAct = scanner.next();
 
             } while (favAct.isEmpty());
 
             do {
                 System.out.println("Finally, a random number between 1 and 50:");
+                while (!scanner.hasNextInt()){
+                    System.out.println("Invalid input. Please try again:");
+                    scanner.next();
+                }
                 randNum = scanner.nextInt();
 
             } while (randNum == 0);
@@ -120,30 +149,18 @@ public class Main {
     }
 
     public static String lottoGen(int num1, int num2, int num3, int num4, int num5, String str ) {
-        String result = "";
-        int[] numbers ={0,0,0,0,0};
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        String result;
         int letter = str.charAt(0);
-        numbers[0] = num1 + num2;
-        numbers[1] = num3 + random2;
-        numbers[2] = 42;
-        numbers[3] =  num4 + random3 + num5;
-        numbers[4] = (letter * random1) / 10;
+        numbers.add( num1 + num2);
+        numbers.add(num3 + random2);
+        numbers.add(42);
+        numbers.add(num4 + random3 + num5);
+        numbers.add((letter * random1) / 10);
 
+        Collections.sort(numbers);
 
-        Arrays.sort(numbers);
-
-        for (int i = 0; i < numbers.length; i++) {
-
-            if (numbers[i] > 66) {
-                System.out.println(numbers[i]);
-                numbers[i] -=65;
-            }
-            if (i < numbers.length - 1) {
-                result += numbers[i] + ", ";
-
-            } else result += numbers[i];
-
-        }
+        result = numbers.toString().replace("[", "").replace("]","");
 
 
 
